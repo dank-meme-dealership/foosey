9372e1a3b7df81cdc09e1d9e17ce0fb5dc11e8cb
Matt Taylor <taylor.matt777@gmail.com>
2016-01-25 10:54:26 -0700

    Fix localStorage bug

--- www/app/local-storage/local-storage.js
+++ www/app/local-storage/local-storage.js
@@ -1,33 +1,31 @@
 angular
   .module('ionic.utils', [])
-  .factory('localStorage', localStorage);
-
-function localStorage($window)
-{
-  return {
-    set       : function (key, value)
-    {
-      $window.localStorage[key] = value;
-    },
-    get       : function (key, defaultValue)
-    {
-      return $window.localStorage[key] || defaultValue;
-    },
-    setObject : function (key, value)
-    {
-      $window.localStorage[key] = JSON.stringify(value);
-    },
-    getObject : function (key)
-    {
-      return $window.localStorage[key] === "undefined" ? {} : JSON.parse($window.localStorage[key] || '{}');
-    },
-    setArray  : function (key, value)
-    {
-      this.setObject(key, value);
-    },
-    getArray  : function (key)
-    {
-      return JSON.parse($window.localStorage[key] || '[]');
-    }
-  };
-}
+  .factory('localStorage', function localStorage($window)
+  {
+    return {
+      set       : function (key, value)
+      {
+        $window.localStorage[key] = value;
+      },
+      get       : function (key, defaultValue)
+      {
+        return $window.localStorage[key] || defaultValue;
+      },
+      setObject : function (key, value)
+      {
+        $window.localStorage[key] = JSON.stringify(value);
+      },
+      getObject : function (key)
+      {
+        return $window.localStorage[key] === "undefined" ? {} : JSON.parse($window.localStorage[key] || '{}');
+      },
+      setArray  : function (key, value)
+      {
+        this.setObject(key, value);
+      },
+      getArray  : function (key)
+      {
+        return JSON.parse($window.localStorage[key] || '[]');
+      }
+    };
+  });
