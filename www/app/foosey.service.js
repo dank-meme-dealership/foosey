7480abe1de037462153386a9b5154b8f29fdd854
Matt Taylor <taylor.matt777@gmail.com>
2016-02-03 23:22:12 -0700

    try new url

--- www/app/foosey.service.js
+++ www/app/foosey.service.js
@@ -1,7 +1,8 @@
 angular.module('foosey')
   .factory('FooseyService', function($http) 
   {
-    var url = "https://run.blockspring.com/api_v2/blocks/foosey?api_key=br_4396_df06230a033b243b55fedbcd7fd3392d847df3bf";
+    var oldUrl = "https://run.blockspring.com/api_v2/blocks/foosey?api_key=br_4396_df06230a033b243b55fedbcd7fd3392d847df3bf";
+    var url = "http://api.foosey.futbol/app";
 
 	  return {
 
