a30054f1920add7fdcf2454f9cb4ca45abdbb1a4
Brik Royster <elkemore@gmail.com>
2016-02-22 13:43:16 -0700

    fixed a bug where foosey add would create a player named add

--- foosey_def.rb
+++ foosey_def.rb
@@ -763,7 +763,7 @@ def log_game_from_slack(user_name, text, trigger_word)
     return make_response('`foosey record` has been renamed `foosey history`')
   elsif text.start_with? 'add'
     return succinct_help unless $admins.include? user_name
-    content = addUser(args[1], content)
+    content = addUser(args[2], content)
     File.write('games.csv', content)
     return make_response('Player added!')
   elsif text.start_with? 'update' and $admins.include? user_name
