850fc1e16ae9a1bb8a6ee1afbcf1d0d5b2b305d3
Matt Taylor <taylor.matt777@gmail.com>
2016-05-03 18:43:48 -0600

    Fix up the service a wee bit

--- www/app/foosey.service.js
+++ www/app/foosey.service.js
@@ -1,97 +1,107 @@
 angular.module('foosey')
   .factory('FooseyService', function($http) 
   {
-    var oldUrl = "https://run.blockspring.com/api_v2/blocks/foosey?api_key=br_4396_df06230a033b243b55fedbcd7fd3392d847df3bf";
     var url = "http://api.foosey.futbol/app";
 
-	  return {
-
-      // adds a game to the foosey csv
-      addGame: function(game)
-      {
-        data =
-        {
-          text: game,
-          user_name: "app",
-          device: "app"
-        }
-        return $http.post(url, data);
-      },
-
-      // get chart data for a player
-      charts: function(name)
-      {
-        data =
-        {
-          text: "charts " + name,
-          device: "app"
-        }
-        return $http.post(url, data);
-      },
-
-      // gets a list of players and their elo ratings
-      leaderboard: function()
-      {
-        data = 
-        {
-          text: 'leaderboard',
-          device: 'app'
-        };
-        return $http.post(url, data);
-      },
-
-      // gets the full game history
-      history: function(start, limit)
-      {
-        data = 
-        {
-          text: "history " + start + " " + limit,
-          device: "app"
-        };
-        return $http.post(url, data);
-      },
-
-      // return a list of players
-      players: function()
-      {
-        data = 
-        {
-          text: "players",
-          device: "app"
-        };
-        return $http.post(url, data);
-      },
-
-      // remove specific game
-      remove: function(id)
-      {
-        data = 
-        {
-          text: "remove " + id,
-          device: "app"
-        };
-        return $http.post(url, data);
-      },
-
-      // get chart data for a player
-      teamCharts: function()
-      {
-        data =
-        {
-          text: "team",
-          device: "app"
-        }
-        return $http.post(url, data);
-      },
-
-      // undo last game
-      undo: function()
-      {
-        data =
-        {
-          text: "undo"
-        }
-        return $http.post(url, data);
-      }
+	  var service =  {
+      addGame: addGame,
+      charts: charts,
+      leaderboard: leaderboard,
+      history: history,
+      players: players,
+      remove: remove,
+      teamCharts: teamCharts,
+      undo: undo
     }
+
+    return service;
+
+    // adds a game to the foosey csv
+    function addGame(game)
+    {
+      data =
+      {
+        text: game,
+        user_name: "app",
+        device: "app"
+      }
+      return $http.post(url, data);
+    }
+
+    // get chart data for a player
+    function charts(name)
+    {
+      data =
+      {
+        text: "charts " + name,
+        device: "app"
+      }
+      return $http.post(url, data);
+    }
+
+    // gets a list of players and their elo ratings
+    function leaderboard()
+    {
+      data = 
+      {
+        text: 'leaderboard',
+        device: 'app'
+      };
+      return $http.post(url, data);
+    }
+
+    // gets the full game history
+    function history(start, limit)
+    {
+      data = 
+      {
+        text: "history " + start + " " + limit,
+        device: "app"
+      };
+      return $http.post(url, data);
+    }
+
+    // return a list of players
+    function players()
+    {
+      data = 
+      {
+        text: "players",
+        device: "app"
+      };
+      return $http.post(url, data);
+    }
+
+    // remove specific game
+    function remove(id)
+    {
+      data = 
+      {
+        text: "remove " + id,
+        device: "app"
+      };
+      return $http.post(url, data);
+    }
+
+    // get chart data for a player
+    function teamCharts()
+    {
+      data =
+      {
+        text: "team",
+        device: "app"
+      }
+      return $http.post(url, data);
+    }
+
+    // undo last game
+    function undo()
+    {
+      data =
+      {
+        text: "undo"
+      }
+      return $http.post(url, data);
+    }
+
   });
