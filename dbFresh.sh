1ee27ff5ff4c047ac636ea4576ca67b3df8edcc4
Matttaylor8910 <taylor.matt777@gmail.com>
2016-06-17 14:07:18 -0700

    Fix dbFresh

--- dbFresh.sh
+++ dbFresh.sh
@@ -3,10 +3,6 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-rm -f foosey.db games.csv
+rm -f foosey.db
 
-wget api.foosey.futbol/games.csv
-
-./csv_to_db.rb
-
-rm -f games.csv
+wget http://api.foosey.futbol/foosey.db
