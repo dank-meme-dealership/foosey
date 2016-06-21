321a3bec5140bacaa54fee48feab722984277705
Matt Taylor <taylor.matt777@gmail.com>
2016-06-21 08:40:25 -0600

    improve build script

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,8 @@
-git pull;
-ionic platform add ios;
-ionic build ios;
+git pull
+rm -rf platforms/
+ionic platform add ios
+ionic build ios
 chmod +x modify_plist.sh
 ./modify_plist.sh
-open platforms/ios/Foosey.xcodeproj;
+open platforms/ios/Foosey.xcodeproj
+open resourses/ios/icon/
