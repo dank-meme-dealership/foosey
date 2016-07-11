d42c03e53f8a91503e6c32bf825eec76b0ed7ade
Matttaylor8910 <taylor.matt777@gmail.com>
2016-07-11 15:12:27 +0300

    Update foosey.routes.js
    Default to leaderboard instead of scorecard

--- app/www/js/foosey.routes.js
+++ app/www/js/foosey.routes.js
@@ -9,7 +9,7 @@
     // If they're logged in, default to leaderboard
     if (SettingsServiceProvider.$get().loggedIn)
     {
-      $urlRouterProvider.otherwise('/app/scorecard');
+      $urlRouterProvider.otherwise('/app/leaderboard');
     } 
     // Otherwise, send them to the login page
     else 
