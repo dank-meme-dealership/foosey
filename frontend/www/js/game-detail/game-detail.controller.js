00f6ad66853e044b6a24ea31a01bd53171550eb3
Matttaylor8910 <taylor.matt777@gmail.com>
2016-11-16 17:56:02 -0700

    Throw a total change on game detail page

--- frontend/www/js/game-detail/game-detail.controller.js
+++ frontend/www/js/game-detail/game-detail.controller.js
@@ -54,10 +54,15 @@
       $scope.teams = _.clone($scope.games[0].teams);
       $scope.teams[0].wins = 0;
       $scope.teams[1].wins = 0;
+      $scope.teams[0].totalChange = 0;
+      $scope.teams[1].totalChange = 0;
       _.each($scope.games, function(game)
       {
         // the first team is always the winner so add a win to whoever it's for
-        game.teams[0].players[0].playerID === $scope.teams[0].players[0].playerID ? $scope.teams[0].wins++ : $scope.teams[1].wins++;
+        var winner = game.teams[0].players[0].playerID === $scope.teams[0].players[0].playerID;
+        $scope.teams[winner ? 0 : 1].wins++;
+        $scope.teams[0].totalChange += game.teams[winner ? 0 : 1].delta;
+        $scope.teams[1].totalChange += game.teams[winner ? 1 : 0].delta;
       });
     }
 
