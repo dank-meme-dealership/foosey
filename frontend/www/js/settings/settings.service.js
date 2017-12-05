2accdc295bcd9e7c7e77f384de41c4a8e3a8ed30
Matttaylor8910 <taylor.matt777@gmail.com>
2017-12-04 22:41:16 -0800

    Truly support iPhone X

--- frontend/www/js/settings/settings.service.js
+++ frontend/www/js/settings/settings.service.js
@@ -8,7 +8,7 @@
   function SettingsService($state, $ionicPopup, $ionicHistory, localStorage) {
     var service = {
       //App Version
-      version: 0.85,
+      version: 0.86,
 
       //Properties
       addGameClear: setting('addGameClear', false),
