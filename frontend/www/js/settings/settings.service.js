76a67d50df80755700cdb7d0003c995393eebbc3
Matttaylor8910 <taylor.matt777@gmail.com>
2018-04-15 19:25:02 -0700

    Bump version number

--- frontend/www/js/settings/settings.service.js
+++ frontend/www/js/settings/settings.service.js
@@ -8,7 +8,7 @@
   function SettingsService($state, $ionicPopup, $ionicHistory, localStorage) {
     var service = {
       //App Version
-      version: 0.89,
+      version: 0.90,
 
       //Properties
       addGameClear: setting('addGameClear', false),
